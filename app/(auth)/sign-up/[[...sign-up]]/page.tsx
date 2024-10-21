21771f7512d65e65b42df6dcb75c2953d024bbaf
Pietro <pietro.dsantos77@gmail.com>
2024-10-21 14:53:17 -0300

    Clerl em português

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,9 +1,23 @@
-import { SignUp } from '@clerk/nextjs';
+import { SignIn } from "@clerk/nextjs";
 
-export default function SignUpPage() {
+export default function SignInPage() {
   return (
-    <main className="flex h-screen w-full items-center justify-center">
-      <SignUp />
+    <main className="flex h-screen w-full items-center justify-center bg-[#F1F5FB]">
+      <SignIn
+        appearance={{
+          variables: {
+            colorPrimary: "#005BAC",
+            colorText: "#003366",
+            colorBackground: "#F8F9FA",
+            colorInputBackground: "#E0E7ED",
+            colorInputText: "#333333",
+          },
+          layout: {
+            socialButtonsVariant: "iconButton",
+            logoImageUrl: "/icons/logo.svg",
+          },
+        }}
+      />
     </main>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,16 @@
 import { ReactNode } from "react";
 import type { Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
-import { Inter } from "next/font/google";
+import { Quicksand } from "next/font/google";
+import { ptBR } from '@clerk/localizations'
+
 
 import "@stream-io/video-react-sdk/dist/css/styles.css";
 import "react-datepicker/dist/react-datepicker.css";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Quicksand({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "UNIDAS - Autochamada",
@@ -23,18 +25,23 @@ export default function RootLayout({
 }: Readonly<{ children: ReactNode }>) {
   return (
     <html lang="pt-br">
-      <ClerkProvider
+      <ClerkProvider localization={ptBR}
         appearance={{
           layout: {
             socialButtonsVariant: "iconButton",
             logoImageUrl: "/icons/logo.svg",
           },
           variables: {
-            colorText: "#fff",
-            colorPrimary: "#0E78F9",
-            colorBackground: "#fff",
-            colorInputBackground: "#F1F5FB",
-            colorInputText: "#000",
+            colorText: "#003366",
+            colorPrimary: "#005BAC",
+            colorBackground: "#F8F9FA",
+            colorInputBackground: "#E0E7ED",
+            colorInputText: "#333333",
+          },
+          elements: {
+            cardTitle: "font-bold text-[#003366]", 
+            formButtonPrimary: "font-semibold text-white bg-[#005BAC]",
+            formFieldLabel: "font-semibold text-[#333333]",
           },
         }}
       >
